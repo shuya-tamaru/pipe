17810625d16fbaac4fa9d50e673a1bc32ea52624
ShuyaTamaru <tamarushuuya@mba.local>
2022-11-23 22:05:01 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { Environment, OrbitControls, useGLTF } from "@react-three/drei";
+import * as THREE from "three";
 
 function App() {
+  const model = useGLTF("/model/pipe.glb");
+  Object.keys(model.materials).forEach((key) => {
+    const material = model.materials[key];
+    material.roughness = 1.0;
+    material.color = new THREE.Color("#008b8b");
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Canvas
+        style={{ width: "100vw", height: "100vh", background: "#333" }}
+        camera={{
+          fov: 75,
+          near: 0.1,
+          far: 200,
+          position: [0, 5, 10],
+        }}
+      >
+        <Environment preset="sunset" />
+        <OrbitControls makeDefault />
+        <primitive object={model.scene} />
+      </Canvas>
+    </>
   );
 }
 
